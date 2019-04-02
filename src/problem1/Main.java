c848c0cf533e720107ced96385ad96e37dd6086f
Dushawn100 <47532450+Dushawn100@users.noreply.github.com>
2019-04-02 10:50:30 -0400

    Loops Assignment

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// Read a value. Print out from 1 to the input
 		Scanner in=new Scanner(System.in);
+		int counter=1;
+		int input=in.nextInt();
+		while (counter<=input) {
+			System.out.println(counter);
+			counter++;
+		}
 	}
 
 }
--- src/problem3/Main.java
+++ src/problem3/Main.java
@@ -5,6 +5,18 @@ public class Main {
 	public static void main(String[] args) {
 		// Read in a word and print out one character per String
 		Scanner in=new Scanner(System.in);
+		String word = "butter";
+		String[] alphabets = word.split("");
+		for(String alphabet : alphabets)
+		    {
+		    System.out.println(alphabet);
+		    }
+		   
+			
+	
+	
+		}
 	}
 
-}
+
+
